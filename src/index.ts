5b97adf6b228a0aa5c51d3513b761786f8f975d7
David Marsh <david@policyinpractice.co.uk>
2020-05-19 09:36:30 +0100

    Start to prediction script

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
-const allWordsByLength = {};
-
-
-
+const allWords = require('../categorised');
 
 const allLetters = 'TLEFVNBARWEENAOESRYNOFEDNSIEEIVITSHRUEDOAHE'.split(''); // I'm lazy don't judge.
-const blanks = ['____', '_', '_', '_________', '___', '_\'__', '________________________']; // 43 in total
+const allBlanks = ['____', '_', '_', '_________', '___', '_\'__', '________________________']; // 43 in total
 
+const fillBlanks = (blanks: string[]) => {
+
+  let availableLetters = allLetters;
+
+    for (let i = 0; i < blanks.length; i++) {
+      if (i == blanks.length-1) {
+        // LAST ONE, PREDICT WORDS
+        console.log('predict words here');
+
+      } else {
+        // We know the layout of this word, let's find a match
+        findMatches(blanks[i], availableLetters)
+      }
+
+    }
+
+};
+
+
+const findMatches = (blank, availableLetters) => {
+  console.log(blank, availableLetters)
+}
+
+
+
+let blankWordStep = 0;
+fillBlanks(allBlanks);
